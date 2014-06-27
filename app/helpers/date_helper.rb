5b118d577aaa51731bf380573048405e27d286b1
Eric Saboia <esaboia@gmail.com>
2014-06-27 01:10:34 -0300

    create helper

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,3 +1,5 @@
 module DateHelper
-
+  def distance_of_time_in_words_or_absolute(date)
+    date <= 2.days.ago ? date.strftime("%b %d, %Y") : time_ago_in_words(date)
+  end
 end
--- spec/helpers/date_helper_spec.rb
+++ spec/helpers/date_helper_spec.rb
@@ -5,22 +5,13 @@ RSpec.describe DateHelper, :type => :helper do
   describe "#distance_of_time_in_words_or_absolute" do
     describe "when date is less then two days ago" do
       it "displays in words" do
-        datetime = Time.now - 4.hours
-        expect(helper.distance_of_time_in_words_or_absolute).to match /about 4 hours ago/
-      end
-    end
-
-    describe "when date is exacly two days ago"  do
-      it "displays in words" do
-        datetime = Time.now - 2.days
-        expect(helper.distance_of_time_in_words_or_absolute).to match /about 2 days ago/
+        expect(helper.distance_of_time_in_words_or_absolute(4.hours.ago)).to match /about 4 hours/
       end
     end
 
     describe "when date is more then two days ago" do
       it "displays in words" do
-        datetime = Time.new(2012,01,12)
-        expect(helper.distance_of_time_in_words_or_absolute).to match /Jan 12, 2012/
+        expect(helper.distance_of_time_in_words_or_absolute(Time.new(2012,01,12))).to match /Jan 12, 2012/
       end
     end
   end
