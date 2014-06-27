69f7cff41ae85de184e154496a4919430039307d
Eric Saboia <esaboia@gmail.com>
2014-06-27 00:13:15 -0300

    add progress bar

--- app/assets/javascripts/twitters.js
+++ app/assets/javascripts/twitters.js
@@ -2,7 +2,9 @@ $(function () {
 
   $('#search-form').submit(function (event) {
     event.preventDefault();
+    NProgress.start();
     $.get($(this).attr('action'), {q: $('#search-text').val()}, function (data) {
+      NProgress.done();
       $('#search-results').html(HandlebarsTemplates['twitters/index']({tweets: data}));
     });
   });
